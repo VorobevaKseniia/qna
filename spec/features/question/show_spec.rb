39496648ecc5b8697a896097277956919969240f
Kseniia <ilijamarmelad@gmail.com>
2024-09-10 21:09:08 +0200

    user can view a question and its answers

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User can view a question and its answers', %q{
+  In order to find an answer to the question
+  As a user
+  I'd like to be able to view a question and its answers
+} do
+
+  given(:question) { create(:question) }
+
+  scenario 'User views question and its answers' do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    question.answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+end

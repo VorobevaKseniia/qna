7a25c4b39be8f68a66b89c53f6aa6eb9b1cddc53
Kseniia <ilijamarmelad@gmail.com>
2024-09-19 18:11:40 +0200

    refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,20 +9,24 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe '.author?' do
+  describe '#author?' do
     let(:author) { create(:user) }
     let(:non_author) { create(:user) }
     let(:question) { create(:question, user: author) }
     let(:answer) { create(:answer, question: question, user: author) }
 
-    it 'returns true if the user is the author of the object' do
-      expect(author.author?(question)).to eq true
-      expect(author.author?(answer)).to eq true
+    context 'when user is the author of object' do
+      it 'returns true' do
+        expect(author.author?(question)).to eq true
+        expect(author.author?(answer)).to eq true
+      end
     end
 
-    it "returns false if the user isn't the author of the object" do
-      expect(non_author.author?(question)).to eq false
-      expect(non_author.author?(answer)).to eq false
+    context 'when user is not author of object' do
+      it 'returns false' do
+        expect(non_author.author?(question)).to eq false
+        expect(non_author.author?(answer)).to eq false
+      end
     end
   end
 end

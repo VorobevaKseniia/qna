7f5b6b5c7d3bf0505197c73e85013414d0324c70
Kseniia <ilijamarmelad@gmail.com>
2024-11-17 22:22:43 +0100

    cleaning

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -23,17 +23,11 @@ class SearchesController < ApplicationController
   private
 
   def search_by_scope(scope, query)
-    Rails.logger.debug "Search by scope called with scope: #{scope}, query: #{query}"
     model = SEARCHABLE_SCOPES[scope]
-    results = model ? model.search_by(query) : []
-    Rails.logger.debug "Search by scope results: #{results}"
-    results
+    model ? model.search_by(query) : []
   end
 
   def search_all(query)
-    Rails.logger.debug "Search all called with query: #{query}"
-    results = SEARCHABLE_SCOPES.values.flat_map { |model| model.search_by(query) }
-    Rails.logger.debug "Search all results: #{results}"
-    results
+    SEARCHABLE_SCOPES.values.flat_map { |model| model.search_by(query) }
   end
 end
